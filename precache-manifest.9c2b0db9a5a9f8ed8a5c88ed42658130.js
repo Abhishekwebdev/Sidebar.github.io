f1e13efae948d54b75296c6d9f1db9c1e0e61e5e
Abhishek Patel <Abhi882731@gmail.com>
2021-08-02 02:27:54 -0700

    Updates

---
+++ precache-manifest.9c2b0db9a5a9f8ed8a5c88ed42658130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1683f340697227219ab0937793c81f71",
+    "url": "/Sidebar.github.io/index.html"
+  },
+  {
+    "revision": "c7adae2d7b68f50ec55c",
+    "url": "/Sidebar.github.io/static/css/main.ec28973a.chunk.css"
+  },
+  {
+    "revision": "84e44487579eef8a3998",
+    "url": "/Sidebar.github.io/static/js/2.6bcace1a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Sidebar.github.io/static/js/2.6bcace1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7adae2d7b68f50ec55c",
+    "url": "/Sidebar.github.io/static/js/main.d84c9094.chunk.js"
+  },
+  {
+    "revision": "47d497968fd087a4fbf5",
+    "url": "/Sidebar.github.io/static/js/runtime-main.685c1495.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/Sidebar.github.io/static/media/logo.2bb7da65.svg"
+  }
+]);
